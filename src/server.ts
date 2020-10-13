f0a469b0f48ed1b8ead74725afd9c8acfa071bbe
Maikon Germano <mk_germano@hotmail.com>
2020-10-13 11:36:58 -0300

    feat: add scripts

--- src/server.ts
+++ src/server.ts
@@ -2,4 +2,10 @@ import express from 'express'
 
 const app = express();
 
+app.use(express.json());
+
+app.get('/users', (request, response) => {
+    return response.json({message: 'Helllo World'})
+})
+
 app.listen(3333);
