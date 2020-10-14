a3a4289bdb473e5c31c8943107dca63a7c7d041d
Maikon Germano <mk_germano@hotmail.com>
2020-10-13 22:12:14 -0300

    feat: add Orphanges_view on file

---
+++ src/views/orphanages_view.ts
@@ -0,0 +1,15 @@
+import Orphanage from "../models/Orphanage";
+
+export default {
+    render(orphanage: Orphanage) {
+        return {
+            name: orphanage.id,
+            latitude: orphanage.latitude,
+            longitude: orphanage.longitude,
+            about: orphanage.about,
+            instructions: orphanage.instructions,
+            opening_hours: orphanage.opening_hours,
+            open_on_weekends: orphanage.open_on_weekends, 
+        };
+    }
+};
