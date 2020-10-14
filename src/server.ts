b7b0a69df193e276ce027baa6f0feb28265abc48
Maikon Germano <mk_germano@hotmail.com>
2020-10-13 22:26:39 -0300

    feat: add route images upload

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,5 @@
 import express from 'express';
+import path from 'path'
 import './database/connection';
 
 import routes from './routes'
@@ -7,6 +8,10 @@ const app = express();
 
 app.use(express.json());
 
+// rotas
 app.use(routes);
+
+// rota para images
+app.use('/uploads', express.static(path.join(__dirname, '..', 'uploads')))
 
 app.listen(3333);
